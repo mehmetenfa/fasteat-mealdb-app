76776e265b05fd8532e3ab73855f154755b3f322
qxan <mehmet.enfa@gmail.com>
2023-04-12 16:16:28 +0300

    app and pages index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import "./App.scss";
+// react router dom
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// pages
+import { Home, MealDetails, Error, Category } from "./pages/index";
+// components
+import Header from "./components/Header/Header";
+import Siderbar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      hop
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Siderbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/meal/:id" element={<MealDetails />} />
+        <Route path="/meal/category/:name" element={<Category />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
