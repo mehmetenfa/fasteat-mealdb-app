579fcb3430b9608ec6566543a00166fcc0dec971
qxan <mehmet.enfa@gmail.com>
2023-04-13 06:25:11 +0300

    searchform first

--- src/components/Header/SearchForm.js
+++ src/components/Header/SearchForm.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./Header.scss";
+import { BiSearch, BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { startFetchMealsBySearch } from "../../actions/mealsActions";
 
 const SearchForm = () => {
-  return (
-    <div>SearchForm</div>
-  )
-}
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
-export default SearchForm
+  const handleSearchTerm = (e) => {
+    e.preventDefault();
+    if (e.target.value.replace(/[^\w\s]/gi, "").length !== 0) {
+      setSearchTerm(e.target.value);
+      setErrorMsg("");
+    } else {
+      setErrorMsg("Invalid search term ...");
+    }
+  };
+
+  const handleSearchResult = (e) => {
+    e.preventDefault();
+    navigate("/");
+    startFetchMealsBySearch(searchTerm);
+  };
+  return (
+    <form
+      className="search-fomr flex align-center"
+      onSubmit={(e) => handleSearchResult(e)}
+    >
+      <input
+        type="text"
+        className="form-control-input text-dark-gray fs-15"
+        placeholder="Search recipes here ..."
+        onChange={(e) => handleSearchTerm(e)}
+      />
+      <button
+        type="submit"
+        className="form-submit-btn text-white text-uppercase fs-14"
+      >
+        <BsSearch className="btn-icon" size={20} />
+      </button>
+    </form>
+  );
+};
+
+export default SearchForm;
