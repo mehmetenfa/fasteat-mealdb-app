ad4f02f628d4a01bf6a7d4742e74bc5880a13460
qxan <mehmet.enfa@gmail.com>
2023-04-13 07:26:46 +0300

    homePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./HomePage.scss";
+import { useMealContext } from "../../context/mealContext";
+import Loader from "../../components/Loader/Loader";
+import CategoryList from "../../components/Category/CategoryList";
+import NotFound from "../../components/NotFound/NotFound";
+import MealList from "../../components/Meal/MealList";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const { categories, meals, categoryLoading, mealsLoading } = useMealContext();
 
-export default HomePage
+  return (
+    <main className="main-content">
+      {mealsLoading ? (
+        <Loader />
+      ) : meals === null ? (
+        <NotFound />
+      ) : meals?.length ? (
+        <MealList meals={meals} />
+      ) : (
+        ""
+      )}
+      {categoryLoading ? <Loader /> : <CategoryList categories={categories} />}
+    </main>
+  );
+};
+
+export default HomePage;
