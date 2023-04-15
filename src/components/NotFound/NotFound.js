0741a3cad945eb590ddd3befa34d78d98909aa5f
qxan <mehmet.enfa@gmail.com>
2023-04-15 07:05:18 +0300

    not found

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./NotFound.scss";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="not-found my-5">
+      <div className="container flex align-center justify-center">
+        No any food found.
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
